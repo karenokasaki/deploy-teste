561a2b8e69fee8e48f9059532c09b10eb1847e02
karen okasaki <oks.karen@gmail.com>
2022-09-29 19:50:57 -0300

    server deploy

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-  production: "LINK DA SUA API DEPLOYADA AQUI",
+  production: "https://projeto3-blog-server.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
